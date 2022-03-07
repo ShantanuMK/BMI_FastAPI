50467c02864bb92f0b7d9f2dccb9ba2ca5115a57
ShantanuMK <59115222+ShantanuMK@users.noreply.github.com>
2022-03-07 15:40:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from router import user
+import uvicorn
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 app.include_router(user.router)
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
+
+if __name__ == "__main__":
+    try:
+        uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    except Exception as e:
+        print(str(e))
